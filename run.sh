b0efd23d51e7fd7159ff9cfe9aee92e74df0fb44
Mubasher693 <mubasheriqbal693@gmail.com>
2022-01-20 23:42:24 +0500

    vendors install

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker-compose --env-file .env -f docker-compose.yml down
 docker-compose --env-file .env -f docker-compose.yml up -d --build
 
-docker-compose run php composer install
+[ -d "./vendor" ] && echo "Vendor exists." || docker-compose run php composer install
